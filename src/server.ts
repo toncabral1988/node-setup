499801b1c0fbbc96de77d34a75edcac5fae43bb8
Ton <washingtonf.nascimento@gmail.com>
2020-06-22 22:05:26 -0400

    Add eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { response } from 'express'
+import express from 'express'
 
 const app = express()
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'})
+  return response.json({ message: 'Hello World' })
 })
 
 app.listen(3333)
